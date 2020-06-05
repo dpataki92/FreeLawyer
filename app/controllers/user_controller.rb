784d594f32af70765f4921b90d88ee0aeb20189a
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-06-05 11:39:49 +0200

    creating signup pages for clients and lawyers

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,18 @@
 class UserController < ApplicationController
+
+    get "/signup/client" do
+        erb :'users/signup_client'
+    end
+
+    get "/signup/lawyer" do
+        erb :'users/signup_lawyer'
+    end
+
+    post "/signup/client" do
+        "You are signed up!"
+    end
+
+    post "/signup/lawyer" do
+        binding.pry
+    end
 end
