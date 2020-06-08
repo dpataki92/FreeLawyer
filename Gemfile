6ec370aae5117c9ef689140d1438719e9ac6df8f
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-06-08 10:26:08 +0200

    adding flash messages with styles

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra'
 gem 'activerecord', '~> 4.2', '>= 4.2.6', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 gem 'rake'
+gem 'sinatra-flash'
 gem 'require_all'
 gem 'sqlite3', '~> 1.3.6'
 gem 'thin'
@@ -11,6 +12,7 @@ gem 'shotgun'
 gem 'pry'
 gem 'bcrypt'
 gem 'tux'
+
 
 group :test do
   gem 'rspec'
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,24 +19,28 @@ class UserController < ApplicationController
     post "/signup/client" do
       
         if !Lawyer.find_by(username: params[:username])
+            flash.now[:success] = "You have successfuly signed up!"
             @client = Client.create(username: params[:username], email: params[:email], password: params[:password])
             session[:user_type] = "client"
             session[:client_id] = @client.id
             
             redirect "/questions/all"
         else
-            "Sorry, this username has already been taken!"
+            flash[:failure] = "Sorry, this username or email has already been taken :("
+            redirect "/signup/client"
         end
     end
 
     post "/signup/lawyer" do
         if !Client.find_by(username: params[:username])
+            flash.now[:success] = "You have successfuly signed up!"
             @lawyer = Lawyer.create(username: params[:username], email: params[:email], password: params[:password], jurisdiction: params[:jurisdiction], expertise: params[:expertise])
             session[:user_type] = "lawyer"
             session[:lawyer_id] = @lawyer.id
             
             redirect "/questions/all"
         else
+            flash[:failure] = "Sorry, this username or email has already been taken :("
             redirect "/signup/lawyer"
         end
     end
@@ -57,13 +61,16 @@ class UserController < ApplicationController
             @client = Client.find_by(username: params[:username])
             if @client && @client.authenticate(params[:password])
                 session[:client_id] = Client.find_by(username: params[:username]).id
+                flash.now[:success] = "You are successfully logged in!"
             end
         elsif params[:user_type] == "lawyer"
             @lawyer = Lawyer.find_by(username: params[:username])
             if @lawyer && @lawyer.authenticate(params[:password])
                 session[:lawyer_id] = Lawyer.find_by(username: params[:username]).id
+                flash.now[:success] = "You are successfully logged in!"
             end
         else
+            flash[:failure] = "Sorry, username or password is invalid :("
             redirect "/login"
         end
         
